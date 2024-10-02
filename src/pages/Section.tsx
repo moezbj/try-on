a54cd61e9996ed885a938765f44807547063380b
moez <moezbj.93@gmail.com>
2024-10-02 23:04:06 +0100

    update  path 2

--- src/pages/Section.tsx
+++ src/pages/Section.tsx
@@ -1,8 +1,10 @@
 export default function TryOn() {
+
+
   return (
     <div>
       <iframe
-        src="../../Virtual-Glasses-Try-on-main/index.html"
+        src="./Virtual-Glasses-Try-on-main/index.html"
         width="100%"
         height="500px"
         style={{ border: "none" }}
